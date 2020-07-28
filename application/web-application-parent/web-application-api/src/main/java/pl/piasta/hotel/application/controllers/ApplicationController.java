9ecd37856cb6c468a7653efa404669910da0f338
geekon <kacperpiasta@gmail.com>
2020-07-28 17:01:07 +0200

    add hello world rest service

---
+++ application/web-application-parent/web-application-api/src/main/java/pl/piasta/hotel/application/controllers/ApplicationController.java
@@ -0,0 +1,16 @@
+package pl.piasta.hotel.application.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> get(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return ResponseEntity.ok("Hello " + name + "!");
+    }
+
+}
