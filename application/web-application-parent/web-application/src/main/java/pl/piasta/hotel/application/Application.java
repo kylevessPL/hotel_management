e3c25c7554f9bd510641f47014a7a4f8db3cd063
geekon <kacperpiasta@gmail.com>
2020-08-05 19:56:56 +0200

    Revert "add additional-services web rest service"
    This reverts commit 67b2e64d3fcee4da12f32b6767d57f21d3635007.

--- application/web-application-parent/web-application/src/main/java/pl/piasta/hotel/application/Application.java
+++ application/web-application-parent/web-application/src/main/java/pl/piasta/hotel/application/Application.java
@@ -2,18 +2,10 @@ package pl.piasta.hotel.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan("pl.piasta.hotel")
-@EntityScan("pl.piasta.hotel.dao.model")
-@EnableJpaRepositories("pl.piasta.hotel.dao.repository")
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
