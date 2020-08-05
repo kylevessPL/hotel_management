3c5af3f5dee4c5fb1c933be1abcb9d56a2b31df9
geekon <kacperpiasta@gmail.com>
2020-08-06 01:45:38 +0200

    code cleanup

--- application/web-application-parent/web-application-infrastructure/src/main/java/pl/piasta/hotel/dao/service/AmenitiesService.java
+++ application/web-application-parent/web-application-infrastructure/src/main/java/pl/piasta/hotel/dao/service/AmenitiesService.java
@@ -1,6 +1,5 @@
 package pl.piasta.hotel.dao.service;
 
-import org.springframework.data.repository.query.Param;
 import pl.piasta.hotel.dao.model.AmenitiesEntity;
 
 import java.util.List;
--- application/web-application-parent/web-application-infrastructure/src/main/java/pl/piasta/hotel/dao/service/CustomersService.java
+++ application/web-application-parent/web-application-infrastructure/src/main/java/pl/piasta/hotel/dao/service/CustomersService.java
@@ -1,10 +1,8 @@
 package pl.piasta.hotel.dao.service;
 
-import org.springframework.data.repository.query.Param;
 import pl.piasta.hotel.dao.model.CustomersEntity;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CustomersService {
 
