3307326227cee95bf9479720a11db73bb2873c84
geekon <kacperpiasta@gmail.com>
2020-08-06 01:55:35 +0200

    add additional-services rest service method to find by name

--- application/web-application-parent/web-application-api/src/main/java/pl/piasta/hotel/application/controllers/ApplicationController.java
+++ application/web-application-parent/web-application-api/src/main/java/pl/piasta/hotel/application/controllers/ApplicationController.java
@@ -2,10 +2,7 @@ package pl.piasta.hotel.application.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.piasta.hotel.application.dateservice.HelloWorldService;
 import pl.piasta.hotel.dao.model.AdditionalServicesEntity;
 import pl.piasta.hotel.dao.service.AdditionalServicesService;
@@ -34,9 +31,14 @@ public class ApplicationController {
         return additionalServicesService.findAll();
     }
 
-    @GetMapping("/hotel/services/additional-services/{id}")
+    @GetMapping("/hotel/services/additional-services/id/{id}")
     public AdditionalServicesEntity findAdditionalServiceById(@PathVariable Integer id) {
         return additionalServicesService.findById(id);
+    }
+
+    @GetMapping("/hotel/services/additional-services/name/{name}")
+    public List<AdditionalServicesEntity> findAdditionalServiceByName(@PathVariable String name) {
+        return additionalServicesService.findByName(name);
     }
 
 }
