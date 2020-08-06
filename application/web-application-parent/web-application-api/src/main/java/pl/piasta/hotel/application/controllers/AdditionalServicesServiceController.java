e0fb7646162c7d826acfe1cccf2c90616e38e3ef
geekon <kacperpiasta@gmail.com>
2020-08-07 01:26:26 +0200

    refactor package names for controllers & separate controller classes

--- application/web-application-parent/web-application-api/src/main/java/pl/piasta/hotel/application/controllers/AdditionalServicesServiceController.java
+++ application/web-application-parent/web-application-api/src/main/java/pl/piasta/hotel/application/controllers/AdditionalServicesServiceController.java
@@ -1,32 +1,22 @@
 package pl.piasta.hotel.application.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pl.piasta.hotel.application.dateservice.HelloWorldService;
+import pl.piasta.hotel.application.dao.service.AdditionalServicesService;
 import pl.piasta.hotel.dao.model.AdditionalServicesEntity;
-import pl.piasta.hotel.dao.service.AdditionalServicesService;
 
 import java.util.List;
 
 @RestController
-public class ApplicationController {
+public class AdditionalServicesServiceController {
 
-    private final HelloWorldService dateService;
     private final AdditionalServicesService additionalServicesService;
 
     @Autowired
-    public ApplicationController(HelloWorldService dateService, AdditionalServicesService additionalServicesService) {
-        this.dateService = dateService;
+    public AdditionalServicesServiceController(AdditionalServicesService additionalServicesService) {
         this.additionalServicesService = additionalServicesService;
-    }
-
-    @GetMapping("/hello")
-    public ResponseEntity<String> get(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return ResponseEntity.ok("Hello " + name + "! Current datetime: " + dateService.getDate());
     }
 
     @GetMapping("/hotel/services/additional-services")
---
+++ application/web-application-parent/web-application-api/src/main/java/pl/piasta/hotel/application/controllers/HelloWorldServiceController.java
@@ -0,0 +1,25 @@
+package pl.piasta.hotel.application.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pl.piasta.hotel.application.dateservice.HelloWorldService;
+
+@RestController
+public class HelloWorldServiceController {
+
+    private final HelloWorldService dateService;
+
+    @Autowired
+    public HelloWorldServiceController(HelloWorldService dateService) {
+        this.dateService = dateService;
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> get(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return ResponseEntity.ok("Hello " + name + "! Current datetime: " + dateService.getDate());
+    }
+
+}
