ab78f93806b70e2072484352c0a14860e70bbd54
geekon <kacperpiasta@gmail.com>
2020-08-06 02:03:10 +0200

    code cleanup

--- application/web-application-parent/web-application-api/src/main/java/pl/piasta/hotel/application/controllers/ApplicationController.java
+++ application/web-application-parent/web-application-api/src/main/java/pl/piasta/hotel/application/controllers/ApplicationController.java
@@ -2,7 +2,10 @@ package pl.piasta.hotel.application.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import pl.piasta.hotel.application.dateservice.HelloWorldService;
 import pl.piasta.hotel.dao.model.AdditionalServicesEntity;
 import pl.piasta.hotel.dao.service.AdditionalServicesService;
