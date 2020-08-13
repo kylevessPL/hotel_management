c67dcaaf8f028546f6676905b116d393f483007c
geekon <kacperpiasta@gmail.com>
2020-08-13 02:21:53 +0200

    create tests for additional-services

--- application/application-dto/src/main/java/pl/piasta/hotel/dto/additionalservices/AdditionalServiceDto.java
+++ application/application-dto/src/main/java/pl/piasta/hotel/dto/additionalservices/AdditionalServiceDto.java
@@ -1,16 +1,18 @@
 package pl.piasta.hotel.dto.additionalservices;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@RequiredArgsConstructor
 @Getter
 @Setter
 public class AdditionalServiceDto {
 
-    private int id;
-    private String name;
-    private BigDecimal price;
+    private final int id;
+    private final String name;
+    private final BigDecimal price;
 
 }
