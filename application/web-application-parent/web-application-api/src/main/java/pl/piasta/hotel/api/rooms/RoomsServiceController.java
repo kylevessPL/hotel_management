610d41f8025040a187d8e4a9131f22ff20f550a3
geekon <kacperpiasta@gmail.com>
2020-08-14 21:20:12 +0200

    ignore whitespace in room rest service sortby param

--- application/web-application-parent/web-application-api/src/main/java/pl/piasta/hotel/api/rooms/RoomsServiceController.java
+++ application/web-application-parent/web-application-api/src/main/java/pl/piasta/hotel/api/rooms/RoomsServiceController.java
@@ -1,9 +1,11 @@
 package pl.piasta.hotel.api.rooms;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,8 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @RestController
@@ -30,9 +34,10 @@ public class RoomsServiceController {
                                                              @RequestParam(name = "sortby", defaultValue = "id", required = false) String sortBy) {
 
         List<String> sortList = Arrays.asList(sortBy.replace("-", "_")
-                                                    .split("[,]"));
+                                                    .split(","));
         List<Sort.Order> sortParams = sortList.stream()
                 .map(s -> new Sort.Order(Sort.Direction.ASC, s))
+                .filter(StringUtils::isEmpty)
                 .collect(Collectors.toList());
 
         return roomMapper.mapToDto(roomsService.getAllAvailableRoomsWithinDateRange(
