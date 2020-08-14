c7517e904dc18bd04070cba0a5128d5b691b327a
geekon <kacperpiasta@gmail.com>
2020-08-14 02:12:33 +0200

    fix dto class constructor annotation pt.2

--- application/application-dto/src/main/java/pl/piasta/hotel/dto/additionalservices/AdditionalServiceDto.java
+++ application/application-dto/src/main/java/pl/piasta/hotel/dto/additionalservices/AdditionalServiceDto.java
@@ -1,10 +1,14 @@
 package pl.piasta.hotel.dto.additionalservices;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class AdditionalServiceDto {
