24efaa83c8c0a4896b5f97fb49613813b06b06e4
geekon <kacperpiasta@gmail.com>
2020-08-14 02:11:02 +0200

    fix dto class constructor annotation pt.2

--- application/application-dto/src/main/java/pl/piasta/hotel/dto/additionalservices/AdditionalServiceDto.java
+++ application/application-dto/src/main/java/pl/piasta/hotel/dto/additionalservices/AdditionalServiceDto.java
@@ -2,12 +2,14 @@ package pl.piasta.hotel.dto.additionalservices;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class AdditionalServiceDto {
--- application/application-dto/src/main/java/pl/piasta/hotel/dto/rooms/RoomDto.java
+++ application/application-dto/src/main/java/pl/piasta/hotel/dto/rooms/RoomDto.java
@@ -2,11 +2,13 @@ package pl.piasta.hotel.dto.rooms;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class RoomDto {
