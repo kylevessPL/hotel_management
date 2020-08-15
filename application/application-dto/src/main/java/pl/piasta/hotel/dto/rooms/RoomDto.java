761b9ba41eb8d3435dacc2c78404eca59907b21c
geekon <kacperpiasta@gmail.com>
2020-08-16 00:39:44 +0200

    change json fields naming convention

--- application/application-dto/src/main/java/pl/piasta/hotel/dto/rooms/RoomDto.java
+++ application/application-dto/src/main/java/pl/piasta/hotel/dto/rooms/RoomDto.java
@@ -1,5 +1,7 @@
 package pl.piasta.hotel.dto.rooms;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +15,16 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE)
 public class RoomDto {
 
+    @JsonProperty(value = "id")
     private int id;
+    @JsonProperty(value = "bed-amount")
     private int bedAmount;
+    @JsonProperty(value = "standard-price")
     private BigDecimal standardPrice;
+    @JsonProperty(value = "amenities")
     private List<AmenityDto> amenities;
 
 }
