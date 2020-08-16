c233923bf826c45eaf02d9509ff5b2153ae97c15
geekon <kacperpiasta@gmail.com>
2020-08-16 02:17:47 +0200

    fix rooms entity dao query being badly handled with jpa sorting

--- application/web-application-parent/web-application-infrastructure/src/main/java/pl/piasta/hotel/infrastructure/dao/RoomsEntityDao.java
+++ application/web-application-parent/web-application-infrastructure/src/main/java/pl/piasta/hotel/infrastructure/dao/RoomsEntityDao.java
@@ -11,12 +11,12 @@ import java.util.List;
 
 public interface RoomsEntityDao extends JpaRepository<RoomsEntity, Integer> {
 
-    @Query(value = "Select rooms.id, room_number, bed_amount, standard_price, name from rooms " +
-            "left join room_amenities " +
-            "on rooms.id = room_amenities.room_id " +
-            "left join amenities " +
-            "on amenities.id = room_amenities.amenity_id " +
-            "where rooms.id not in " +
+    @Query(value = "Select r.id, room_number, bed_amount, standard_price, name from rooms r " +
+            "left join room_amenities e " +
+            "on r.id = e.room_id " +
+            "left join amenities a " +
+            "on a.id = e.amenity_id " +
+            "where r.id not in " +
             "(Select room_id from bookings " +
             "where start_date >= :start_date and end_date <= :end_date)",
             nativeQuery = true)
