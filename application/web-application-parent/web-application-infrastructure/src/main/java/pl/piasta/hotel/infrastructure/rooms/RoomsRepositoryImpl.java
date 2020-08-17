95400436b39b8bc1458efb02f83375effdcd3843
geekon <kacperpiasta@gmail.com>
2020-08-17 18:55:30 +0200

    clumsy me

--- application/web-application-parent/web-application-infrastructure/src/main/java/pl/piasta/hotel/infrastructure/rooms/RoomsRepositoryImpl.java
+++ application/web-application-parent/web-application-infrastructure/src/main/java/pl/piasta/hotel/infrastructure/rooms/RoomsRepositoryImpl.java
@@ -3,32 +3,24 @@ package pl.piasta.hotel.infrastructure.rooms;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
-import pl.piasta.hotel.domain.model.amenities.Amenity;
 import pl.piasta.hotel.domain.model.rooms.Room;
 import pl.piasta.hotel.domain.rooms.RoomsRepository;
 import pl.piasta.hotel.infrastructure.dao.RoomsEntityDao;
+import pl.piasta.hotel.infrastructure.rooms.mapper.RoomsEntityMapper;
 
 import java.sql.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
 public class RoomsRepositoryImpl implements RoomsRepository {
 
+    private final RoomsEntityMapper roomsEntityMapper;
     private final RoomsEntityDao dao;
 
     @Override
     public List<Room> getAllAvailableRoomsWithinDateRange(Date startDate, Date endDate, Pageable pageable) {
-        return dao.findAllAvailableWithinDateRange(startDate, endDate, pageable).stream()
-                .map(entity -> new Room(
-                        entity.getId(),
-                        entity.getBedAmount(),
-                        entity.getStandardPrice(),
-                        entity.getAmenitiesEntities().stream()
-                                .map(amenity -> new Amenity(amenity.getName()))
-                                .collect(Collectors.toList())))
-                .collect(Collectors.toList());
+        return roomsEntityMapper.mapToRoom(dao.findAllAvailableWithinDateRange(startDate, endDate, pageable));
     }
 
 }
