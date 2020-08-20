c472770108da26993410ca516b92ca31538caf19
geekon <kacperpiasta@gmail.com>
2020-08-20 19:06:03 +0200

    fix dao method being badly annotated

--- application/web-application-parent/web-application-infrastructure/src/main/java/pl/piasta/hotel/infrastructure/dao/BookingsEntityDao.java
+++ application/web-application-parent/web-application-infrastructure/src/main/java/pl/piasta/hotel/infrastructure/dao/BookingsEntityDao.java
@@ -2,7 +2,7 @@ package pl.piasta.hotel.infrastructure.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.repository.query.Param;
 import pl.piasta.hotel.infrastructure.model.BookingsEntity;
 
 import java.sql.Date;
@@ -11,6 +11,6 @@ import java.util.List;
 public interface BookingsEntityDao extends JpaRepository<BookingsEntity, Integer> {
 
     @Query("select bookings.roomId from BookingsEntity bookings where bookings.startDate >= :startDate and bookings.endDate <= :endDate")
-    List<Integer> findRoomIdBetweenDates(@RequestParam("startDate") Date startDate, @RequestParam("endDate") Date endDate);
+    List<Integer> findRoomIdBetweenDates(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
 
 }
