f6768504ad5947dcb7a955c437a356162f64fbcf
geekon <kacperpiasta@gmail.com>
2020-08-21 00:49:00 +0200

    Revert "change persisting room amenities logic"
    This reverts commit c24ddc7e3f5da534a1fb16a440dbfdcd2f082e0c.

--- application/web-application-parent/web-application-infrastructure/src/main/java/pl/piasta/hotel/infrastructure/dao/AmenitiesEntityDao.java
+++
@@ -1,7 +0,0 @@
-package pl.piasta.hotel.infrastructure.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.piasta.hotel.infrastructure.model.AmenitiesEntity;
-
-public interface AmenitiesEntityDao extends JpaRepository<AmenitiesEntity, Integer> {
-}
